f417d37e9137e3a87477dbfeaa4c29415d0b5576
342619939 <342619939@C-1B0VHB2.tdsb.on.ca>
2025-05-21 10:46:56 -0400

    added get pass in account class

--- src/Account.java
+++ src/Account.java
@@ -44,6 +44,9 @@ public class Account {
     public void setUsername(String username) {
         this.username = username;
     }
+    public String getPassword() {
+        return password;
+    }
     public void setPassword(String password) {
         this.password = password;
     }
